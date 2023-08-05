fdc3e6f54fb419b3fe6576d7390d4a9d30ab945f
Domenico Cucinotta <domenico.cucinotta@external.cnhind.com>
2023-08-05 12:07:31 +0200

    add css baseline main.jsx and removed vite default css settings

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { createTheme } from "@mui/material/styles";
 import { amber, teal } from "@mui/material/colors";
 import { ThemeProvider } from "@emotion/react";
+import CssBaseline from '@mui/material/CssBaseline';
 
 const theme = createTheme({
   palette: {
@@ -17,6 +18,7 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline /> 
       <App />
     </ThemeProvider>
   </React.StrictMode>
